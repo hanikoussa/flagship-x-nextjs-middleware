915c39d26cc20c6aee4c43a9cf0a03225df9c59b
hani koussa <36161665+hanikoussa@users.noreply.github.com>
2023-01-25 21:17:40 +0100

    first message

---
+++ pages/posts/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link"
+
+export default function Posts({ posts }) {
+    return <>
+        <h1>Posts list</h1> {
+            posts.map(post => {
+                return (
+                    <div key={post.id}>
+                        <Link href={`posts/${post.id}`} passHref>
+                            <h2>{post.id} {post.title}</h2>
+                        </Link>
+                        <hr />
+                    </div>
+                )
+            })
+        }
+
+    </>
+
+}
+
+
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data.slice(0, 3)
+        }
+    }
+}
